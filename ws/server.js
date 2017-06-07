2edc8b575e5898eba427283193392ab2d554aaf9
TomGoldberg <tomg3210@gmail.com>
2017-06-07 21:41:53 +0300

    but fix

--- ws/server.js
+++ ws/server.js
@@ -17,6 +17,11 @@ const http = require('http'),
 app.use(express.static(mainRoute));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+app.use( (req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 /*
 * default rout
